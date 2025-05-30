29daae013fec62360d50af6084a40792c6bb95b4
Suraj Verma <thesurajsite@gmail.com>
2025-05-30 18:30:45 +0530

    lets change the world

---
+++ database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// connect() establishes a connection to MongoDB  and returns the client and collections
+func Connect() (*mongo.Client, *mongo.Collection) {
+
+	// connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// MongoDB connection string
+	connectionString := "example"
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize Collections
+	db := client.Database("nita_buddy")
+	userCollection := db.Collection("users")
+
+	// check connection by running a query
+	err = userCollection.FindOne(ctx, bson.M{}).Err()
+	if err != nil && err != mongo.ErrNoDocuments {
+		log.Fatalf("Failer to query user Collection: %v", err)
+	}
+
+	log.Println("Successfully connected to NITA Buddy Database")
+	return client, userCollection
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module gitHub.com/suraj/nitabuddy
+
+go 1.23.4
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	go.mongodb.org/mongo-driver v1.17.3 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
